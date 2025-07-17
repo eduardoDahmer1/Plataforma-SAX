abf138d8a390a39f9786bbd6c22b49b879068851
eduardoDahmer1 <eduardoluizdahmer@gmail.com>
2025-07-17 13:47:24 -0300

    master

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'channel' => env('LOG_CHANNEL', 'daily'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,8 +69,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
-            'replace_placeholders' => true,
+            'days' => 8,  // mantém os últimos 8 arquivos
         ],
 
         'slack' => [

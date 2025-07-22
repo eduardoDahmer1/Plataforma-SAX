67a25a970f987dd213b2fa2c97652ca0f39c215b
eduardoDahmer1 <eduardoluizdahmer@gmail.com>
2025-07-22 16:57:54 -0300

    master

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,9 @@
-<footer class="bg-light text-center p-3">
-    <div class="container">
-        <p class="mb-0">&copy; {{ date('Y') }} Todos os direitos reservados.</p>
+<footer class="bg-light text-center p-3 d-grid justify-content-center align-items-center">
+    <p class="mb-0">&copy; {{ date('Y') }} Todos os direitos reservados.</p>
+    @if ($webpImage)
+    <div>
+        <img src="{{ asset('storage/uploads/' . $webpImage) }}" alt="Imagem Header"
+            style="max-height: 150px; display: block; margin-bottom: 10px;">
     </div>
+    @endif
 </footer>

539389e583222676a6fa6ad3ae6f65b9b0e796ff
eduardoDahmer1 <eduardoluizdahmer@gmail.com>
2025-09-13 12:18:17 -0300

    master

--- resources/views/Childcategory/show.blade.php
+++ resources/views/Childcategory/show.blade.php
@@ -119,7 +119,7 @@
                                         @if (in_array(auth()->user()->user_type, [0, 1, 2]))
                                             <form action="{{ route('cart.addAndCheckout') }}" method="POST" class="d-flex">
                                                 @csrf
-                                                <input type="hidden" name="product_id" value="{{ $item->id }}">
+                                                <input type="hidden" name="product_id" value="{{ $product->id }}">
                                                 <button type="submit" class="btn btn-sm btn-primary flex-grow-1">
                                                     <i class="fas fa-bolt me-1"></i> Comprar Agora
                                                 </button>

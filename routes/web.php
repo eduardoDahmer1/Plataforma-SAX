6993f5ab9c8168738ab4499d7458c495e464c9b5
eduardoDahmer1 <eduardoluizdahmer@gmail.com>
2026-02-25 09:41:49 -0300

    master

--- routes/web.php
+++ routes/web.php
@@ -139,16 +139,16 @@ Route::middleware('auth')->group(function () {
 
     Route::delete('/user/delete', [UserController::class, 'destroy'])->name('user.destroy');
 
-    // Verification
-    Route::get('/email/verify', fn() => view('auth.verify-email'))->name('verification.notice');
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-        return redirect('/home');
-    })->middleware(['signed'])->name('verification.verify');
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-        return back()->with('message', 'Link de verificação reenviado!');
-    })->middleware(['throttle:6,1'])->name('verification.send');
+    // // Verification
+    // Route::get('/email/verify', fn() => view('auth.verify-email'))->name('verification.notice');
+    // Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    //     $request->fulfill();
+    //     return redirect('/home');
+    // })->middleware(['signed'])->name('verification.verify');
+    // Route::post('/email/verification-notification', function (Request $request) {
+    //     $request->user()->sendEmailVerificationNotification();
+    //     return back()->with('message', 'Link de verificação reenviado!');
+    // })->middleware(['throttle:6,1'])->name('verification.send');
 });
 
 // --- Admin Routes ---
